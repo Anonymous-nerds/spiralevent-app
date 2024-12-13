2904a9e45685c540ac1ac5b0ae88864cbc3478a4
anasyakubu <yakubuanas04@gmail.com>
2024-12-13 20:22:30 +0100

    refactor some files

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,17 @@
 import React from "react";
-import { SiVite } from "react-icons/si";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { IoLogoSass } from "react-icons/io5";
-import { FaReact, FaGithub, FaNpm } from "react-icons/fa";
-import { SiAxios, SiPrettier } from "react-icons/si";
-
-import { Star } from "lucide-react";
-
-function Home() {
-import React from 'react'
-import Nav from "../Nav.jsx"
-import Footer from "../Footer.jsx"
+import Nav from "../../components/Nav.jsx";
+import Footer from "../../components/Footer.jsx";
 
 const Home = () => {
   return (
     <div>
-      <div className='overflow-x-hidden'>
+      <div className="overflow-x-hidden">
         <Nav />
 
         <Footer />
       </div>
     </div>
-  )
-} 
+  );
+};
 
 export default Home;
-
-export default Home
-
-  
