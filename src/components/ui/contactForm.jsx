88d564e3f696ffc8621ca3722ea2d066d721d4ad
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2024-12-18 23:18:55 +0100

    register page modified

--- src/components/ui/contactForm.jsx
+++ src/components/ui/contactForm.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import logo from '../../assets/spiraleE4.png'
 
 const contactForm = () => {
   return (
     <div>
       <form className="max-w-lg mx-auto bg-gray-50 rounded-2xl shadow-xl m-8 p-8">
-        <h2 className="text-3xl font-bold mb-6 text-center text-pink-900">
-          Contact Us
+        {/* <h3 className="text-3xl md:text-4xl font-bold flex gap-3 text-gray-800 mb-2">
+          <img src={logo} alt="" className="h-10" />{" "}
+          <span>
+            Spiral
+            <span className="text-pink-900">Event</span>
+          </span>
+        </h3> */}
+
+        <h2 className="text-2xl font-bold mb-4 text-pink-900 flex justify-center">
+          Create an Account
         </h2>
+
         <div className="space-y-4">
           <div className="grid md:grid-cols-2 gap-4">
             <div>
@@ -36,17 +46,28 @@ const contactForm = () => {
             placeholder="Enter Email"
             className="w-full border border-gray-300 rounded-lg p-3 focus:ring-2 focus:ring-pink-900 focus:outline-none"
           />
-          <textarea
-            placeholder="Enter Message"
-            rows="5"
+          <label className="block text-gray-700 font-bold mb-2">
+            Create Password:
+          </label>
+          <input
+            type="password"
+            placeholder="create password"
             className="w-full border border-gray-300 rounded-lg p-3 focus:ring-2 focus:ring-pink-900 focus:outline-none"
-          ></textarea>
+          />
+          <label className="block text-gray-700 font-bold mb-2">
+            Confirm Password:{" "}
+          </label>
+          <input
+            type="password"
+            placeholder="confirm password"
+            className="w-full border border-gray-300 rounded-lg p-3 focus:ring-2 focus:ring-pink-900 focus:outline-none"
+          />
         </div>
         <button
           type="submit"
-          className="w-1/3 mx-auto mt-6 bg-pink-900 text-white py-3 rounded-lg hover:bg-pink-700 hover:scale-105 shadow-lg transition-transform duration-300"
+          className="w-1/3 mx-auto mt-6 bg-pink-900 text-white py-3 rounded-lg flex justify-center hover:bg-pink-700 hover:scale-105 shadow-lg transition-transform duration-300"
         >
-          Send Message
+          Sign Up
         </button>
       </form>
     </div>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react'
+import heroimg from '../../assets/register.svg';
+import logo from '../../assets/spiraleE4.png'
+import Pif from '../../components/ui/contactForm'
 
 const Register = () => {
-  return <div>Register</div>;
-};
+  return (
+    <div className="h-screen w-full flex flex-col lg:flex-row">
+      <div className="w-full h-screen lg:w-1/2 p-6 md:p-12 bg-neutral-100 flex items-center justify-center">
+        <div className="w-full max-w-md">
+          <Pif/>
 
-export default Register;
+        </div>
+      </div>
+
+      
+      <div className="hidden lg:block lg:w-1/2 h-screen">
+        <img src={heroimg} alt="Event" className="w-full h-full object-cover" />
+      </div>
+    </div>
+  );
+}
+
+export default Register
